b32d37a8ce0bb377096bf289a32a8ae88a510647
Keysi Jones <keysijones01@gmail.com>
2021-06-10 19:03:21 -0300

    Integrando com api do mercado pago para conseguir as assinaturas.

--- src/main/java/com/example/authentication/controller/AuthController.java
+++ src/main/java/com/example/authentication/controller/AuthController.java
@@ -17,15 +17,22 @@ import com.example.authentication.repository.UserRepository;
 import com.example.authentication.security.jwt.JwtUtils;
 import com.example.authentication.service.impl.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -45,7 +52,7 @@ public class AuthController {
     JwtUtils jwtUtils;
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
 
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
@@ -57,12 +64,17 @@ public class AuthController {
                 .map(item -> item.getAuthority())
                 .collect(Collectors.toList());
 
-        return ResponseEntity.ok()
-                .body(new JwtResponse(jwt,
-                        userDetails.getId(),
-                        userDetails.getUsername(),
-                        userDetails.getEmail(),
-                        roles));
+        final Cookie cookie = new Cookie("logged-user-email", loginRequest.getUsername());
+        cookie.setHttpOnly(true);
+        //cookie.setSecure(true);
+        response.addCookie(cookie);
+        //cookie.setMaxAge(maxAge);
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
     }
 
     @PostMapping("/cadastrar")
--- src/main/java/com/example/authentication/controller/TestController.java
+++ src/main/java/com/example/authentication/controller/TestController.java
@@ -1,14 +1,28 @@
 package com.example.authentication.controller;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+    @Value("${MERCADOTOKEN}")
+    private String TOKEN;
+
     @GetMapping("/all")
     public String allAccess() {
         return "Public Content.";
@@ -30,5 +44,17 @@ public class TestController {
     @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
     public String adminAccess() {
         return "Admin Board.";
+    }
+
+    @GetMapping("/admin/subscriptions")
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
+    public String subscriptions(@RequestParam String email) throws URISyntaxException, IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        URI uri = new URIBuilder("https://api.mercadopago.com/preapproval/search?payer_email=" + email).build();
+        HttpGet httpGet = new HttpGet(uri);
+        httpGet.setHeader("Authorization", "BEARER " + TOKEN);
+        CloseableHttpResponse response = client.execute(httpGet);
+
+        return EntityUtils.toString(response.getEntity());
     }
 }
